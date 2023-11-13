887bbbebc5770e54f88cff08d43831dd6fde9e77
alvisxp <128601501+alvisxp@users.noreply.github.com>
2023-11-13 00:38:54 -0500

    add tests

--- get-function/main_test.go
+++ get-function/main_test.go
@@ -1,60 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestHandler(t *testing.T) {
-	testCases := []struct {
-		name          string
-		request       events.APIGatewayProxyRequest
-		expectedBody  string
-		expectedError error
-	}{
-		{
-			// mock a request with an empty SourceIP
-			name: "empty IP",
-			request: events.APIGatewayProxyRequest{
-				RequestContext: events.APIGatewayProxyRequestContext{
-					Identity: events.APIGatewayRequestIdentity{
-						SourceIP: "",
-					},
-				},
-			},
-			expectedBody:  "Hello, world!\n",
-			expectedError: nil,
-		},
-		{
-			// mock a request with a localhost SourceIP
-			name: "localhost IP",
-			request: events.APIGatewayProxyRequest{
-				RequestContext: events.APIGatewayProxyRequestContext{
-					Identity: events.APIGatewayRequestIdentity{
-						SourceIP: "127.0.0.1",
-					},
-				},
-			},
-			expectedBody:  "Hello, 127.0.0.1!\n",
-			expectedError: nil,
-		},
+	request := events.APIGatewayProxyRequest{
+		Path:       "get-function/",
+		HTTPMethod: "GET",
 	}
 
-	for _, testCase := range testCases {
-		t.Run(testCase.name, func(t *testing.T) {
-			response, err := handler(testCase.request)
-			if err != testCase.expectedError {
-				t.Errorf("Expected error %v, but got %v", testCase.expectedError, err)
-			}
+	response, err := handler(request)
 
-			if response.Body != testCase.expectedBody {
-				t.Errorf("Expected response %v, but got %v", testCase.expectedBody, response.Body)
-			}
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+	assert.Equal(t, 200, response.StatusCode)
 
-			if response.StatusCode != 200 {
-				t.Errorf("Expected status code 200, but got %v", response.StatusCode)
-			}
-		})
-	}
+	var responseBody map[string]string
+	err = json.Unmarshal([]byte(response.Body), &responseBody)
+	assert.NoError(t, err)
+
+	expectedCount := responseBody["count"]
+	assert.Equal(t, expectedCount, responseBody["count"])
 }
--- put-function/main_test.go
+++ put-function/main_test.go
@@ -1,60 +1,92 @@
 package main
 
 import (
+	"errors"
+	"log"
+	"reflect"
 	"testing"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
-func TestHandler(t *testing.T) {
-	testCases := []struct {
-		name          string
-		request       events.APIGatewayProxyRequest
-		expectedBody  string
-		expectedError error
-	}{
-		{
-			// mock a request with an empty SourceIP
-			name: "empty IP",
-			request: events.APIGatewayProxyRequest{
-				RequestContext: events.APIGatewayProxyRequestContext{
-					Identity: events.APIGatewayRequestIdentity{
-						SourceIP: "",
-					},
-				},
+type MockDynamoDBClient struct {
+	dynamodbiface.DynamoDBAPI
+	UpdateItemOutput dynamodb.UpdateItemOutput
+	UpdateItemError  error
+}
+
+func (m *MockDynamoDBClient) UpdateItem(input *dynamodb.UpdateItemInput) (*dynamodb.UpdateItemOutput, error) {
+	return &m.UpdateItemOutput, m.UpdateItemError
+}
+
+func t_handler(request events.APIGatewayProxyRequest, svc dynamodbiface.DynamoDBAPI) (events.APIGatewayProxyResponse, error) {
+	input := &dynamodb.UpdateItemInput{
+		TableName: aws.String("cloud-resume-challenge"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"ID": {
+				S: aws.String("visitors"),
 			},
-			expectedBody:  "Hello, world!\n",
-			expectedError: nil,
 		},
-		{
-			// mock a request with a localhost SourceIP
-			name: "localhost IP",
-			request: events.APIGatewayProxyRequest{
-				RequestContext: events.APIGatewayProxyRequestContext{
-					Identity: events.APIGatewayRequestIdentity{
-						SourceIP: "127.0.0.1",
-					},
-				},
+		UpdateExpression: aws.String("ADD visitors :inc"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":inc": {
+				N: aws.String("1"),
 			},
-			expectedBody:  "Hello, 127.0.0.1!\n",
-			expectedError: nil,
 		},
 	}
 
-	for _, testCase := range testCases {
-		t.Run(testCase.name, func(t *testing.T) {
-			response, err := handler(testCase.request)
-			if err != testCase.expectedError {
-				t.Errorf("Expected error %v, but got %v", testCase.expectedError, err)
+	_, err := svc.UpdateItem(input)
+
+	if err != nil {
+		log.Printf("Got error calling UpdateItem: %s", err)
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	return events.APIGatewayProxyResponse{}, nil
+}
+
+func TestHandler(t *testing.T) {
+	tests := []struct {
+		name             string
+		dynamoDBClient   dynamodbiface.DynamoDBAPI
+		expectedResponse events.APIGatewayProxyResponse
+		expectedError    error
+	}{
+		{
+			name: "Successfully update item",
+			dynamoDBClient: &MockDynamoDBClient{
+				UpdateItemOutput: dynamodb.UpdateItemOutput{},
+				UpdateItemError:  nil,
+			},
+			expectedResponse: events.APIGatewayProxyResponse{},
+			expectedError:    nil,
+		},
+		{
+			name: "Error updating item",
+			dynamoDBClient: &MockDynamoDBClient{
+				UpdateItemOutput: dynamodb.UpdateItemOutput{},
+				UpdateItemError:  errors.New("DynamoDB update error"),
+			},
+			expectedResponse: events.APIGatewayProxyResponse{},
+			expectedError:    errors.New("DynamoDB update error"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			response, err := t_handler(events.APIGatewayProxyRequest{}, tt.dynamoDBClient)
+
+			if !reflect.DeepEqual(response, tt.expectedResponse) {
+				t.Errorf("Expected response %v, but got %v", tt.expectedResponse, response)
 			}
 
-			if response.Body != testCase.expectedBody {
-				t.Errorf("Expected response %v, but got %v", testCase.expectedBody, response.Body)
-			}
-
-			if response.StatusCode != 200 {
-				t.Errorf("Expected status code 200, but got %v", response.StatusCode)
+			if err != nil && (tt.expectedError == nil || err.Error() != tt.expectedError.Error()) {
+				t.Errorf("Expected error %v, but got %v", tt.expectedError, err)
 			}
 		})
+
 	}
 }
